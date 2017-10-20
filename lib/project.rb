40db9a9448d9c72c9d75650b7f81381e52237a85
Adam Fudge <coolfudge@gmail.com>
2017-10-20 18:47:47 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -6,6 +6,10 @@ class Project
   end
 
   def add_backer(backer)
-    @backers << backer
+
+    if !(@backers.find {|copy| copy == backer})
+      @backers << backer
+      backer.back_project(self)
+    end
   end
 end
